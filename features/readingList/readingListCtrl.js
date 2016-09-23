30ed5943dc050072a0a291ad6f294f94f09bea36
JJ Crossman <jj.crossman@gmail.com>
2016-09-23 18:58:23 -0500

    pushing changes

--- features/readingList/readingListCtrl.js
+++ features/readingList/readingListCtrl.js
@@ -1,9 +1,6 @@
 module.exports = {
   addBookToList( req, res ) {
     if ( req.session.hasOwnProperty( "readingList" ) ) {
-      if ( req.session.readingList[0] === undefined ) {
-        req.session.readingList.shift();
-      }
       req.session.readingList.push( req.body );
       return res.status( 200 ).json( req.session.readingList );
     } else {
